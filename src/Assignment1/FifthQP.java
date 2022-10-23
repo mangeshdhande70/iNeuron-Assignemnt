1baffdeb7cf84106fbe0e4ca7a7ec39d3432b48e
mangeshdhande70 <mangeshdhande70@gmail.com>
2022-10-23 08:05:33 +0530

    First Assignment added

---
+++ src/Assignment1/FifthQP.java
@@ -0,0 +1,16 @@
+package Assignment1;
+
+public class FifthQP {
+    public static void main(String[] args) {
+        int n = 10;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <n ; j++) {
+                if (i+j<=(n-1)/2 || i==0 || j==0|| i-j>=(n-1)/2 || i==n-1)
+                    System.out.print("* ");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
